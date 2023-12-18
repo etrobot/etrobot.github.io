50bd756d55f0c0722e477969d45cbbb1fba60e9f
tfproduct01 <etrobot@outlook.com>
2023-12-18 08:47:45 +0800

    daily

--- Bash/hugo.sh
+++ Bash/hugo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git config --global user.email "etrobot@outlook.com"
+git config remote.origin.url "https://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@github.com/etrobot/cfpage"
 curPath="$(dirname "$0")"
 cd $curPath || exit
 cd .. && /opt/homebrew/bin/hugo -D && git add . && git commit -m 'daily' && git push
